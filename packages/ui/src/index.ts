16dae51f7013d6e662e9bcc2328e646540490acd
AllenHUSH <allenshhu@outlook.com>
2025-09-16 15:51:42 +0800

    refactor(ui): 重构组件导出结构，明确区分基础组件和增强组件
    将基础组件从统一的导出文件中拆分出来单独导出，并移除已在增强组件中封装的组件导出

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,4 +1,22 @@
 import "./styles/global.css"
 
-export * from './components/base'
+export * from '@/components/base/badge'
+export * from '@/components/base/dialog'
+// export * from '@/components/base/select' // 在 enhanced 有封装
+export * from '@/components/base/skeleton'
+export * from '@/components/base/button'
+export * from '@/components/base/dropdown-menu'
+export * from '@/components/base/input'
+export * from '@/components/base/separator'
+export * from '@/components/base/tabs'
+export * from '@/components/base/card'
+export * from '@/components/base/form'
+export * from '@/components/base/label'
+export * from '@/components/base/sheet'
+// export * from '@/components/base/tooltip' // 在 enhanced 有封装
+export * from '@/components/base/collapsible'
+export * from '@/components/base/hover-card'
+export * from '@/components/base/popover'
+export * from '@/components/base/sidebar'
+
 export * from './components/enhanced'
