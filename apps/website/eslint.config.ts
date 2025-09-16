7b00550043fab5e3878b58e6773b3fbae092f596
AllenHUSH <allenshhu@outlook.com>
2025-09-16 17:34:37 +0800

    refactor(eslint): 调整eslint配置结构以提升可读性
    将vue规则配置从文件匹配配置中分离出来，使配置结构更清晰

--- apps/website/eslint.config.ts
+++ apps/website/eslint.config.ts
@@ -1,6 +1,6 @@
 import { globalIgnores } from 'eslint/config'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
-import pluginVue, { rules } from 'eslint-plugin-vue'
+import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
@@ -12,14 +12,16 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
-    rules: {
-      'vue/multi-word-component-names': 'off'
-    }
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
   pluginVue.configs['flat/essential'],
+  {
+    rules: {
+      'vue/multi-word-component-names': 'off'
+    }
+  },
   vueTsConfigs.recommended,
   skipFormatting,
 )
