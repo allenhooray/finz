2a53f955054bd53327b2239364ab4f7a9efd3734
AllenHUSH <allenshhu@outlook.com>
2025-09-16 15:42:57 +0800

    feat(HelloWorld): 使用Card组件重构页面布局
    重构HelloWorld组件，使用Card、CardHeader和CardContent组件替代原有简单div布局，提升UI一致性和美观性

--- playground/ui-playground/src/components/HelloWorld.vue
+++ playground/ui-playground/src/components/HelloWorld.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-// import { Button } from '@finz/ui/components/base/button'
-import { Button } from '@finz/ui'
+import { Button, Card, CardContent, CardHeader } from '@finz/ui'
 
 defineProps<{ msg: string }>()
 
@@ -9,26 +8,31 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+  <Card>
+    <CardHeader>
+      <h1>{{ msg }}</h1>
+    </CardHeader>
 
-  <div class="card">
-    <Button @click="count++">count is {{ count }}</Button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
-  </div>
+    <CardContent>
+      <Button @click="count++">count is {{ count }}</Button>
+      <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test HMR
+      </p>
+      <p>
+        Check out
+        <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
+        starter
+      </p>
+      <p>
+        Learn more about IDE Support for Vue in the
+        <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up
+          Guide</a>.
+      </p>
+      <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
+    </CardContent>
+  </Card>
 
-  <p>
-    Check out
-    <a href="https://vuejs.org/guide/quick-start.html#local" target="_blank">create-vue</a>, the official Vue + Vite
-    starter
-  </p>
-  <p>
-    Learn more about IDE Support for Vue in the
-    <a href="https://vuejs.org/guide/scaling-up/tooling.html#ide-support" target="_blank">Vue Docs Scaling up Guide</a>.
-  </p>
-  <p class="read-the-docs">Click on the Vite and Vue logos to learn more</p>
 </template>
 
 <style scoped>
